395a42ee99a8b9624c7bc39c2528c9fa6b7d1f96
matosimi <matosimi@33423248-3bca-264f-8ce8-56538b218e63>
2022-09-07 11:25:32 +0000

    added AtariMapConverter to convert old binary format of atrmap to new json format
    git-svn-id: svn+ssh://svn.code.sf.net/p/atari-mapmaker/code@34 33423248-3bca-264f-8ce8-56538b218e63

---
+++ AtariMapConverter/OldAtrmapDataProvider.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace AtariMapMaker
+{
+    [Serializable]
+    public class AtariMap
+    {
+        public byte[] data;
+        public int[] Data { get { return data.Select(b => (int)b).ToArray(); } }
+        public Size screenSize;    
+        public Size screens;       
+    }
+
+    [Serializable]
+    public class AtariFontRenderer
+    {
+        public byte[] fontData;
+        public int[] FontData { get { return fontData.Select(b => (int)b).ToArray(); } }
+        public byte[] color5;
+        public int[] Color5 { get { return color5.Select(b => (int)b).ToArray(); } }
+    }
+
+    internal class OldAtrmapDataProvider
+    {
+        private AtariMap myMap;
+        private AtariFontRenderer myRenderer;
+
+        public void OpenAtrmap(string fileName)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            System.IO.FileStream fs = new System.IO.FileStream(fileName, System.IO.FileMode.Open); //test.dat
+            myMap = (AtariMap)bf.Deserialize(fs);
+            myRenderer = (AtariFontRenderer)bf.Deserialize(fs);
+            fs.Close();
+        }
+        public  byte[] GetFontData()
+        {
+            return myRenderer.fontData;
+        }
+
+        public  byte[] GetColor5()
+        {
+            return myRenderer.color5;
+        }
+
+        public  byte[] GetMapData()
+        {
+            return myMap.data;
+        }
+
+        public  Size GetMapSize()
+        {
+            return myMap.screens;
+        }
+
+        public  Size GetScreenSize()
+        {
+            return myMap.screenSize;
+        }
+
+        public  string FlushData()
+        {
+
+            JsonObject jo = new JsonObject
+            {
+                { "AtrmapVersion", "1.1"},
+                { "MapData", JsonValue.Create(myMap.Data) },
+                { "MapSize", JsonValue.Create(myMap.screens) },
+                { "MapScreenSize", JsonValue.Create(myMap.screenSize) },
+                { "FontData", JsonValue.Create(myRenderer.FontData) },
+                { "Color5", JsonValue.Create(myRenderer.Color5) }
+            };
+
+            string jsonString = JsonSerializer.Serialize(jo);
+            return jsonString;
+        }
+    }
+}
---
+++ AtariMapConverter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtariMapMaker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("AtariMapConverter 1.0 (7.9.2022) for legacy AtariMapMaker (atrmap) binary format (version 1.1 or lower).\n");
+                Console.WriteLine("Usage: AtariMapConverter myMap.atrmap");
+                Console.WriteLine("Usage: AtariMapConverter myMap.atrmap > newMap.atrmap");
+
+                return;
+            }
+            OldAtrmapDataProvider oadp = new OldAtrmapDataProvider();
+            oadp.OpenAtrmap(args[0]);
+            Console.Write(oadp.FlushData());
+        }
+    }
+}
